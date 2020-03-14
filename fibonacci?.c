25566a72209c8bb855f4fcbbefa742e011f45347
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2020-03-14 12:24:31 +0800

    Create fibonacci?.c

---
+++ fibonacci?.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+ int main()
+ {
+ 	long long int f1=1,f2=2;
+ 	int i;
+ 	for(i=1;i<=35;i++)
+ 	{
+	 printf("%30lld %30lld",f1,f2);
+	 if(i%2==0) printf("\n");
+	 f1=f1+f2;
+	 f2=f2+f1;}
+   return 0;
+ 	
+   
+}
