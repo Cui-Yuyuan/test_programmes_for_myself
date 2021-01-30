d55256639b9bab88f90d3a0a0f934ae8ffc359bb
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2021-01-30 21:00:17 +0800

    Create pzy's_program(pointers).c

---
+++ pzy's_program(pointers).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main(void)
+ {
+    u[5]={100,200,300,400,500};
+    *ptr1,*ptr2,*ptr3;
+    ptr1=u;
+    ptr2=&u[2];
+    ptr3=ptr1+4
+    printf("ptrval,dereferencedptr,ptradd:");
+    printf("ptr1=%p,*ptr1=%d,&ptr1=%p\n",ptr1,*ptr1,&ptr1);
+    printf("add an interger to a pointer");
+    }
