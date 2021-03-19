aaa2a2ed3bc205c0834a828d89d6bbdf20dd2864
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2021-03-19 23:51:54 +0800

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,33 +1,43 @@
 import random,turtle,math
-a=turtle.Pen()
-a.speed(0)
+print("input create number(row),num(line),num(size)")
+x=10
+y=5
+s=20
+print("input start position(x,y),using enter to separate two numbers")
+stx=-200
+sty=-180
+print("input pensize")
+psz=8
 def stps(c,b):
-    import turtle
-    turtle.penup()
-    turtle.setposition(a,b)
-    turtle.pendown()
-stps(-500,-500)
-size=10;x=20;y=10;c=["#afff79","#eeee01","#e03729b","#039982"]
-s=size
+	import turtle
+	turtle.penup()
+	turtle.setposition(c,b)
+	turtle.pendown()
+	
 def fd2(s):
-    import turtle
     turtle.fd(2*s)
+    
 def fds2(s):
-    import turtle,math
     turtle.fd(math.sqrt(2)*s)
+    
 def cubegen(x,s):
-    import turtle,math,rantdom
-    for p in range(x):
-        a.pencolor(random.choice(c))
+    for __ in range(x):
+        turtle.pencolor(random.choice(col))
         for _ in range(4):
-            fd2(s);a.lt(90)
-        fd2(s);a.lt(45)
-        fds2(s);a.lt(135)
-        fd2(s);a.lt(135)
-        fds2(s);a.lt(45)
-        fd2(s);a.lt(45)
-        fds2(s);a.rt(135)
+            fd2(s);turtle.lt(90)
+        fd2(s);turtle.lt(45)
+        fds2(s);turtle.lt(135)
+        fd2(s);turtle.lt(45)
+        fds2(s);turtle.rt(135)
+        fd2(s);turtle.rt(45)
+        fds2(s);turtle.rt(45)
         fd2(s)
-for j in range(y):
+        
+turtle.speed(0)
+turtle.pensize(10)
+
+stps(stx,sty)
+col=["#afff79","#81efa8","#8ade70","#8eefac","#eeee01","#e0729b","#039982"]
+for j in range(1,1+y):
     cubegen(x,s)
-    stps(-500-3*j*s,-500-3*j*s)
+    stps(stx+3*j*s,sty+j*s)
