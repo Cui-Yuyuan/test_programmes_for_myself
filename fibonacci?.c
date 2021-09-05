ac5bce524048055c1db25d261ee794ca14a0fe0e
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2021-09-06 00:22:02 +0800

    fibonacci1.c

--- fibonacci?.c
+++ fibonacci?.c
@@ -1,16 +1,17 @@
 include<stdio.h>
  int main()
  {
- 	long long int f1=1,f2=2;
+ 	unsigned long long int f1=1,f2=2;
  	int i;
  	for(i=1;i<=35;i++)
  	{
 	 printf("%30lld %30lld",f1,f2);
-	 if(i%2==0) printf("\n");
 	 f1=f1+f2;
 	 f2=f2+f1;}
    return 0;
 //notice:
+//here is just a possible way.i have written
+//another programme which also works.
 //the program comes from a book 
-//just ignore what I said  
+//just ignore what I said 
 }
