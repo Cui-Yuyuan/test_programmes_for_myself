a71c74355604fbf7bf735ee7d7133003313ffc78
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2022-01-07 10:21:28 +0000

    Create fibonacci1.c

---
+++ c&cpp/fibonacci1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+ {
+ 	unsigned long long int f1=1,f2=2;
+ 	int i;
+ 	for(i=1;i<=35;i++)
+ 	{
+	 printf("%30lld %30lld",f1,f2);
+	 f1=f1+f2;
+	 f2=f2+f1;}
+   return 0;
+//notice:
+//here is just a possible way.i have written
+//another programme which also works.
+//the program comes from a book 
+//just ignore what I said 
+}
