aa7a081a1957543189dbf780eee7224e973b2357
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2022-01-07 10:26:48 +0000

    Create fibonacci2.c

---
+++ c&cpp/fibonacci2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+	double an=0,an1-1,an2=0;
+	int S=0;
+	prnitf("1\n");
+	while(S<1476)//If the number is above 1476,more useless nums,naming inf,will be print.
+	{
+		an2=an+an1;
+		an=an1;
+		an1=an2;
+		S+=1;
+		printf("%f\n",an2);}
+	return 0;}
