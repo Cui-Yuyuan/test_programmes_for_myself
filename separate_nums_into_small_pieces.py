9ac886838d6f48267f2d6049cf1275a88d78654e
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2022-01-07 09:56:14 +0000

    Create separate_nums_into_small_pieces.py

--- separate_nums_into_small_pieces.py
+++ separate_nums_into_small_pieces.py
@@ -1,9 +1,10 @@
 import math 
-a=int(input())
-i=2
-while (i<=a)==True:
-	if (a%i)==0:
-		print(i,end=" ")
-		a/=i
-	else:
-		i+=1
+while 1:
+	a=int(input())
+	i=2
+	while (i<=a)==True:
+		if (a%i)==0:
+			print(i,end=" ")
+			a/=i
+		else:
+			i+=1
