114f7ad8e22b7823229e65eb9e8593431be71a30
Cui-Yuyuan <44122522+Cui-Yuyuan@users.noreply.github.com>
2022-01-07 10:33:58 +0000

    Create test01.py

---
+++ py/test01.py
@@ -0,0 +1,14 @@
+import turtle
+t=turtle.Pen()
+t.fillcolor("#268282")
+t.begin_fill()
+t.fd(100)
+t.right(90)
+t.fd(100)
+t.right(90)
+t.fd(100)
+t.right(90)
+t.fd(100)
+t.right(90)
+t.end_fill()
+#A colorful square. 
